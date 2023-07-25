dddda08bdf8d14e93e7c873b7293ea1a40ee1c84
Chinchu Jose <40754520+chinchu1913@users.noreply.github.com>
2023-07-25 09:31:15 +0200

    Connection

--- app/src/main/java/com/example/bluetoothconnection/data/AndroidBluetoothController.kt
+++ app/src/main/java/com/example/bluetoothconnection/data/AndroidBluetoothController.kt
@@ -4,15 +4,25 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothServerSocket
+import android.bluetooth.BluetoothSocket
 import android.content.Context
 import android.content.IntentFilter
 import android.content.pm.PackageManager
+import com.example.bluetoothconnection.domain.ConnectionResult
 import com.example.bluetoothconnection.domain.chat.BluetoothController
 import com.example.bluetoothconnection.domain.chat.BluetoothDeviceDomain
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.update
+import java.io.IOException
+import java.util.UUID
 
 @SuppressLint("MissingPermission")
 class AndroidBluetoothController(private val context: Context) : BluetoothController {
@@ -21,6 +31,74 @@ class AndroidBluetoothController(private val context: Context) : BluetoothContro
     }
     private val bluetoothAdapter by lazy {
         bluetoothManager?.adapter
+    }
+
+    private var currentServiceSocket: BluetoothServerSocket? = null
+
+    private var currentClientSocket: BluetoothSocket? = null
+
+    override fun startBluetoothServer(): Flow<ConnectionResult> {
+        return flow {
+            if (!hasPermission(Manifest.permission.BLUETOOTH_CONNECT)) {
+                throw SecurityException("No Bluetooth Permission ")
+            }
+            currentServiceSocket = bluetoothAdapter?.listenUsingInsecureRfcommWithServiceRecord(
+                "chat_service", UUID.fromString(
+                    SERVICE_UUID
+                )
+            )
+
+            var shouldLoop = true
+            while (shouldLoop) {
+                currentClientSocket = try {
+                    currentServiceSocket?.accept()
+
+                } catch (e: IOException) {
+                    shouldLoop = false
+                    null
+                }
+                emit(ConnectionResult.ConnectionEstablished)
+                currentClientSocket?.let {
+                    currentServiceSocket?.close()
+                }
+            }
+        }.onCompletion {
+            closeConnection()
+        }.flowOn(Dispatchers.IO)
+    }
+
+    override fun connectToDevice(device: com.example.bluetoothconnection.domain.chat.BluetoothDevice): Flow<ConnectionResult> {
+        return flow {
+            if (!hasPermission(Manifest.permission.BLUETOOTH_CONNECT)) {
+                throw SecurityException("No Bluetooth Permission ")
+            }
+            currentClientSocket = bluetoothAdapter?.getRemoteDevice(device.address)
+                ?.createRfcommSocketToServiceRecord(
+                    UUID.fromString(
+                        SERVICE_UUID
+                    )
+                )
+            stopDiscovery()
+            currentClientSocket?.let { socket ->
+                try {
+                    socket.connect()
+                    emit(ConnectionResult.ConnectionEstablished)
+                } catch (e: IOException) {
+                    socket.close()
+                    currentClientSocket = null
+                    emit(ConnectionResult.Error("Connection was inptrittes"))
+                }
+            }
+        }.onCompletion {
+            closeConnection()
+        }.flowOn(Dispatchers.IO)
+    }
+
+    override fun closeConnection() {
+        currentClientSocket?.close()
+        currentServiceSocket?.close()
+        currentServiceSocket=null
+        currentClientSocket=null
     }
 
     private val _scannedDevice = MutableStateFlow<List<BluetoothDeviceDomain>>(emptyList())
@@ -63,6 +141,7 @@ class AndroidBluetoothController(private val context: Context) : BluetoothContro
 
     override fun release() {
         context.unregisterReceiver(foundDataReceiver)
+        closeConnection()
     }
 
     @SuppressLint("MissingPermission")
@@ -77,5 +156,9 @@ class AndroidBluetoothController(private val context: Context) : BluetoothContro
 
     private fun hasPermission(permission: String): Boolean {
         return context.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        const val SERVICE_UUID = "3739580e-27c6-11ee-be56-0242ac120002"
     }
 }
--- app/src/main/java/com/example/bluetoothconnection/domain/chat/BluetoothController.kt
+++ app/src/main/java/com/example/bluetoothconnection/domain/chat/BluetoothController.kt
@@ -1,12 +1,19 @@
 package com.example.bluetoothconnection.domain.chat
 
+import com.example.bluetoothconnection.domain.ConnectionResult
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 
 interface BluetoothController {
-    val scannedDevice:StateFlow<List<BluetoothDevice>>
-    val pairedDevice:StateFlow<List<BluetoothDevice>>
+    val scannedDevice: StateFlow<List<BluetoothDevice>>
+    val pairedDevice: StateFlow<List<BluetoothDevice>>
 
     fun startDiscovery()
     fun stopDiscovery()
     fun release()
+    fun startBluetoothServer(): Flow<ConnectionResult>
+    fun connectToDevice(device: BluetoothDevice): Flow<ConnectionResult>
+
+    fun closeConnection()
+
 }
